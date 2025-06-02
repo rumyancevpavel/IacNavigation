7a0e2f5b4bac2ec013345dda484e6149ae283ff3
Pavel Rumyancev <paulru-ext@monday.com>
2025-06-02 11:50:40 +0300

    Ellen running OK

---
+++ Assets/IacNavigation/6-Character/Scripts/EllenBehaviour.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace IacNavigation.Character.Scripts
+{
+	public class EllenBehaviour : MonoBehaviour
+	{
+		#region Consts
+		
+		private const string RUNNING_PARAM_NAME = "Running";
+		
+		#endregion
+		
+		#region Editor
+
+		[SerializeField] private NavMeshAgent _agent;
+
+		[SerializeField] private Animator _animator;
+
+		#endregion
+
+		#region Methods
+
+		public void GoTo(Vector3 destination)
+		{
+			_agent.SetDestination(destination);
+		}
+
+		private void Update()
+		{
+			_animator.SetBool(RUNNING_PARAM_NAME, IsMoving());
+		}
+
+		private bool IsMoving()
+		{
+			return _agent.velocity.magnitude > 0.1f;
+		}
+
+		private void OnAnimatorMove()
+		{
+			if (_animator.GetBool(RUNNING_PARAM_NAME))
+			{
+				_agent.speed = (_animator.deltaPosition / Time.deltaTime).magnitude;
+			}
+		}
+
+		#endregion
+	}
+}
